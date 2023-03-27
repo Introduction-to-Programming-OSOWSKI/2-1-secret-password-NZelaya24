637395cf586e2a36ee039d901b8aae2e1aeeb4a9
NZelaya24 <825123@mystma.org>
2023-03-27 14:17:19 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+def password (p):
+        if p == "Knights19":
+            print ("ACCESS GRANTED")
+
+        else:
+            print ("ACCESS DENIED")
+
+
+password ("password")
+password ("Knights19")
+
